80dff5f0cdf9f1ab8964c4a128561fed6366b387
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-11 21:25:48 +0100

    move data-related methods from Fragment to ViewModel

--- app/src/main/java/com/clouddroid/usagesafe/fragments/TodaysStatsFragment.kt
+++ app/src/main/java/com/clouddroid/usagesafe/fragments/TodaysStatsFragment.kt
@@ -1,10 +1,21 @@
 package com.clouddroid.usagesafe.fragments
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.clouddroid.usagesafe.adapters.MostUsedAdapter
+import com.clouddroid.usagesafe.models.AppUsageInfo
 import com.clouddroid.usagesafe.viewmodels.TodaysStatsViewModel
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.MPPointF
+import kotlinx.android.synthetic.main.fragment_todays_stats.*
 
 
 class TodaysStatsFragment : BaseFragment() {
@@ -20,10 +31,78 @@ class TodaysStatsFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getTodaysStats()
+        viewModel.init()
+        observeDataChanges()
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProviders.of(activity!!, viewModelFactory)[TodaysStatsViewModel::class.java]
+    }
+
+    private fun observeDataChanges() {
+        viewModel.getAppUsageMap().observe(this, Observer {
+            drawChart(it)
+            setChartCenterText(viewModel.getTotalScreenTime(it))
+            setUpMostUsedRV(viewModel.getMostUsedAppsList(it))
+        })
+
+        viewModel.unlockCount.observe(this, Observer {
+            setUnlockText(it)
+        })
+    }
+
+    private fun setUpMostUsedRV(list: List<AppUsageInfo>) {
+        mostUsedRV.adapter = MostUsedAdapter(list)
+        val dividerItemDecoration = DividerItemDecoration(
+            mostUsedRV.context, LinearLayoutManager.VERTICAL
+        )
+        mostUsedRV.addItemDecoration(dividerItemDecoration)
+    }
+
+    private fun setUnlockText(unlockCount: Int?) {
+        unlockCountTV.text = unlockCount.toString()
+    }
+
+    private fun setChartCenterText(totalScreenTimeText: String) {
+        pieChart.centerText = "Total screen time: \n$totalScreenTimeText"
+    }
+
+    private fun drawChart(appUsageMap: Map<String, AppUsageInfo>) {
+        val entries = viewModel.prepareEntriesForPieChart(appUsageMap, context)
+        val pieDataSet = prepareDataSet(entries)
+        fillAndCustomizeChart(pieDataSet)
+    }
+
+
+    private fun prepareDataSet(entries: List<PieEntry>): PieDataSet {
+        val pieDataSet = PieDataSet(entries, "App usage")
+        pieDataSet.sliceSpace = 3f
+        pieDataSet.colors = viewModel.generateColorsFromAppIcons(entries)
+        pieDataSet.isHighlightEnabled = false
+        pieDataSet.iconsOffset = MPPointF(0f, 32f)
+        pieDataSet.valueTextSize = 0f
+        pieDataSet.xValuePosition = PieDataSet.ValuePosition.OUTSIDE_SLICE
+        pieDataSet.valueLinePart1OffsetPercentage = 0.5f
+        pieDataSet.valueLinePart1Length = 0.15f
+        pieDataSet.valueLinePart2Length = 0f
+        pieDataSet.valueLineColor = Color.TRANSPARENT
+        pieDataSet.setAutomaticallyDisableSliceSpacing(true)
+        return pieDataSet
+    }
+
+    private fun fillAndCustomizeChart(pieDataSet: PieDataSet) {
+        val pieData = PieData(pieDataSet)
+        pieChart.data = pieData
+        pieChart.holeRadius = 90f
+        pieChart.isRotationEnabled = false
+        pieChart.description.isEnabled = false
+        pieChart.legend.isEnabled = false
+        pieChart.extraBottomOffset = 20f
+        pieChart.extraTopOffset = 20f
+        pieChart.setEntryLabelColor(Color.BLACK)
+        pieChart.setEntryLabelTextSize(14f)
+        pieChart.setCenterTextSize(16f)
+        pieChart.animateXY(500, 500)
+        pieChart.invalidate()
     }
 }
--- app/src/main/java/com/clouddroid/usagesafe/viewmodels/TodaysStatsViewModel.kt
+++ app/src/main/java/com/clouddroid/usagesafe/viewmodels/TodaysStatsViewModel.kt
@@ -1,12 +1,113 @@
 package com.clouddroid.usagesafe.viewmodels
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.core.graphics.drawable.toBitmap
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import androidx.palette.graphics.Palette
+import com.clouddroid.usagesafe.models.AppUsageInfo
 import com.clouddroid.usagesafe.repositories.UsageStatsRepository
+import com.clouddroid.usagesafe.utils.PackageInfoUtils.getAppIcon
+import com.clouddroid.usagesafe.utils.TextUtils.getTotalScreenTimeText
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class TodaysStatsViewModel @Inject constructor(private val usageStatsRepository: UsageStatsRepository) : ViewModel() {
 
-    fun getTodaysStats() {
-        usageStatsRepository.getAppsUsageFromToday()
+    private val appUsageMap = MutableLiveData<Map<String, AppUsageInfo>>()
+    val unlockCount = MutableLiveData<Int>()
+
+    fun init() {
+        val usagePair = usageStatsRepository.getAppsUsageFromToday()
+        appUsageMap.value = usagePair.first
+        unlockCount.value = usagePair.second
+
     }
+
+    fun getAppUsageMap(): LiveData<Map<String, AppUsageInfo>> = Transformations.map(appUsageMap) {
+        it.toList()
+            .sortedByDescending { (_, value) ->
+                value.totalTimeInForeground
+            }
+            .toMap()
+    }
+
+    fun getTotalScreenTime(appUsageMap: Map<String, AppUsageInfo>): String {
+        return getTotalScreenTimeText(appUsageMap.toList().sumBy { it.second.totalTimeInForeground.toInt() }.toLong())
+    }
+
+    fun getMostUsedAppsList(appUsageMap: Map<String, AppUsageInfo>): List<AppUsageInfo> {
+        return appUsageMap.toList().take(5).toMap().values.toList()
+    }
+
+    fun prepareEntriesForPieChart(appUsageMap: Map<String, AppUsageInfo>, context: Context?): List<PieEntry> {
+        val firstAppsUsageMap = getFirstNonZeroAppsUsage(appUsageMap)
+        val restAppsUsageMap = getOtherAppsUsage(appUsageMap, firstAppsUsageMap.size)
+        return getPieEntriesFromAppsUsage(firstAppsUsageMap, restAppsUsageMap, context)
+    }
+
+    private fun getFirstNonZeroAppsUsage(appUsageMap: Map<String, AppUsageInfo>): Map<String, AppUsageInfo> {
+        val firstAppsUsageMap = mutableMapOf<String, AppUsageInfo>()
+        appUsageMap.toList().forEachIndexed { index, pair ->
+            if (index < 5 && pair.second.totalTimeInForeground > 0) {
+                firstAppsUsageMap[pair.first] = pair.second
+            }
+        }
+        return firstAppsUsageMap
+    }
+
+    private fun getOtherAppsUsage(appUsageMap: Map<String, AppUsageInfo>, beginIndex: Int): Map<String, AppUsageInfo> {
+        return appUsageMap.toList().subList(beginIndex, appUsageMap.size).toMap()
+    }
+
+    private fun getPieEntriesFromAppsUsage(
+        firstAppsUsageMap: Map<String, AppUsageInfo>,
+        restAppsUsageMap: Map<String, AppUsageInfo>,
+        context: Context?
+    ): List<PieEntry> {
+
+        val entries = mutableListOf<PieEntry>()
+
+        firstAppsUsageMap.forEach { (packageName, usageInfo) ->
+            entries.add(
+                PieEntry(
+                    TimeUnit.MILLISECONDS.toMinutes(usageInfo.totalTimeInForeground).toFloat(),
+                    "",
+                    getAppIcon(packageName, context)
+                )
+            )
+        }
+
+        entries.add(
+            PieEntry(
+                TimeUnit.MILLISECONDS.toMinutes(
+                    restAppsUsageMap.toList().sumBy { it.second.totalTimeInForeground.toInt() }.toLong()
+                ).toFloat(),
+                "Other"
+            )
+        )
+
+        return entries
+    }
+
+    fun generateColorsFromAppIcons(entries: List<PieEntry>): MutableList<Int>? {
+        val colors = mutableListOf<Int>()
+        entries.forEach { entry ->
+            if (entry.icon != null) {
+                val palette = createPaletteSync(entry.icon.toBitmap(80, 80))
+                colors.add(palette.getDominantColor(ColorTemplate.MATERIAL_COLORS.random()))
+            } else {
+                colors.add(Color.GRAY)
+            }
+        }
+        return colors
+    }
+
+    private fun createPaletteSync(bitmap: Bitmap): Palette = Palette.from(bitmap).generate()
 }
