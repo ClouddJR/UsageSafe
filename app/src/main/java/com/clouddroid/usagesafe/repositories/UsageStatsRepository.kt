badcf3c369384d4a9944d4d68d75ca2d72df969f
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-11 21:26:23 +0100

    split huge getAppsUsageFromToday method into smaller chunks

--- app/src/main/java/com/clouddroid/usagesafe/repositories/UsageStatsRepository.kt
+++ app/src/main/java/com/clouddroid/usagesafe/repositories/UsageStatsRepository.kt
@@ -2,18 +2,18 @@ package com.clouddroid.usagesafe.repositories
 
 import android.app.usage.UsageEvents
 import android.app.usage.UsageStatsManager
-import android.util.Log
+import com.clouddroid.usagesafe.models.AppUsageInfo
 import java.util.*
-import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class UsageStatsRepository @Inject constructor(private val usageStatsManager: UsageStatsManager) {
 
 
-    fun getAppsUsageFromToday(): Map<String, AppUsageInfo> {
+    fun getAppsUsageFromToday(): Pair<Map<String, AppUsageInfo>, Int> {
 
-        val appUsageMap = mutableMapOf<String, UsageStatsRepository.AppUsageInfo>()
+        var unlockCount = 0
         val allEventsList = mutableListOf<UsageEvents.Event>()
+        val appUsageMap = mutableMapOf<String, AppUsageInfo>()
 
         val beginCalendar = Calendar.getInstance().apply {
             set(Calendar.HOUR_OF_DAY, 0)
@@ -25,10 +25,24 @@ class UsageStatsRepository @Inject constructor(private val usageStatsManager: Us
             set(Calendar.MINUTE, 59)
         }
 
-        val events = usageStatsManager.queryEvents(beginCalendar.timeInMillis, endCalendar.timeInMillis)
+        getAllRelevantEventsToList(
+            usageStatsManager.queryEvents(beginCalendar.timeInMillis, endCalendar.timeInMillis),
+            allEventsList
+        )
 
-        var unlockCount = 0
+        for (i in 0 until allEventsList.size - 1) {
+            val first = allEventsList[i]
+            val second = allEventsList[i + 1]
 
+            unlockCount += possiblePhoneUnlock(first, second)
+            increaseLaunchCount(first, second, appUsageMap)
+            increaseTotalTimeInForeground(first, second, appUsageMap)
+        }
+
+        return Pair(appUsageMap, unlockCount)
+    }
+
+    private fun getAllRelevantEventsToList(events: UsageEvents?, allEventsList: MutableList<UsageEvents.Event>) {
         events?.let {
             while (events.hasNextEvent()) {
                 val currentEvent = UsageEvents.Event()
@@ -41,54 +55,54 @@ class UsageStatsRepository @Inject constructor(private val usageStatsManager: Us
                 }
             }
         }
-
-        for (i in 0 until allEventsList.size - 1) {
-            val first = allEventsList[i]
-            val second = allEventsList[i + 1]
-
-            //calculating number of phone unlocks
-            if (first.packageName == second.packageName
-                && first.eventType == UsageEvents.Event.MOVE_TO_BACKGROUND
-                && second.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
-            ) {
-                unlockCount++
-            }
-
-            //calculating number of times opened for each app
-            if (first.packageName != second.packageName
-                && second.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
-            ) {
-                if (appUsageMap[second.packageName] == null) {
-                    appUsageMap[second.packageName] = AppUsageInfo()
-                }
-                appUsageMap[second.packageName]!!.launchCount++
-            }
-
-            //calculating total time in foreground for each app
-            if (first.className == second.className && first.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
-                && second.eventType == UsageEvents.Event.MOVE_TO_BACKGROUND
-            ) {
-                if (appUsageMap[second.packageName] == null) {
-                    appUsageMap[second.packageName] = AppUsageInfo()
-                }
-                appUsageMap[second.packageName]!!.totalTimeInForeground += (second.timeStamp - first.timeStamp)
-            }
-        }
-
-        Log.d("UsageStatsUnlock", unlockCount.toString())
-        appUsageMap.forEach { (packageName, appUsageInfo) ->
-            Log.d(
-                "UsageStats",
-                "$packageName- opened: ${appUsageInfo.launchCount}, " +
-                        "time: ${TimeUnit.MILLISECONDS.toMinutes(appUsageInfo.totalTimeInForeground)}"
-            )
-        }
-
-        return appUsageMap
     }
 
+    private fun possiblePhoneUnlock(first: UsageEvents.Event, second: UsageEvents.Event): Int {
+        return if (first.packageName == second.packageName
+            && first.className == second.className
+            && first.eventType == UsageEvents.Event.MOVE_TO_BACKGROUND
+            && second.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
+        ) {
+            1
+        } else {
+            0
+        }
+    }
 
-    class AppUsageInfo(var launchCount: Int = 0, var totalTimeInForeground: Long = 0)
+    private fun increaseLaunchCount(
+        first: UsageEvents.Event,
+        second: UsageEvents.Event,
+        appUsageMap: MutableMap<String, AppUsageInfo>
+    ) {
+        if (first.packageName != second.packageName
+            && second.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
+        ) {
+            if (appUsageMap[second.packageName] == null) {
+                appUsageMap[second.packageName] = AppUsageInfo().apply {
+                    packageName = second.packageName
+                }
+            }
+            appUsageMap[second.packageName]!!.launchCount++
+        }
+
+    }
+
+    private fun increaseTotalTimeInForeground(
+        first: UsageEvents.Event,
+        second: UsageEvents.Event,
+        appUsageMap: MutableMap<String, AppUsageInfo>
+    ) {
+        if (first.className == second.className && first.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
+            && second.eventType == UsageEvents.Event.MOVE_TO_BACKGROUND
+        ) {
+            if (appUsageMap[second.packageName] == null) {
+                appUsageMap[second.packageName] = AppUsageInfo().apply {
+                    packageName = second.packageName
+                }
+            }
+            appUsageMap[second.packageName]!!.totalTimeInForeground += (second.timeStamp - first.timeStamp)
+        }
+    }
 }
 
 
