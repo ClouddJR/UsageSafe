d604f7f95bcd0d4885809c8227598e0dc29857cc
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-15 19:08:30 +0100

    add extension functions to FragmentManager and FragmentTransaction classes

--- app/src/main/java/com/clouddroid/usagesafe/utils/ExtensionUtils.kt
+++ app/src/main/java/com/clouddroid/usagesafe/utils/ExtensionUtils.kt
@@ -1,5 +1,32 @@
 package com.clouddroid.usagesafe.utils
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.clouddroid.usagesafe.R
+import com.clouddroid.usagesafe.fragments.BaseFragment
+
 object ExtensionUtils {
+
+//****************Fragment Manager**********************************************************************
+
+    fun FragmentManager.doesNotContain(fragment: BaseFragment): Boolean {
+        return !this.fragments.contains(fragment)
+    }
+
+    fun FragmentManager.addAndCommit(fragment: BaseFragment) {
+        this.beginTransaction()
+            .add(R.id.fragmentPlaceHolder, fragment)
+            .commit()
+    }
+
+    fun FragmentTransaction.showAndHideOthers(fragment: BaseFragment, fragments: List<Fragment>) {
+        this.show(fragment)
+        fragments.forEach {
+            if (it != fragment) this.hide(it)
+        }
+    }
+
+//****************Fragment Manager**********************************************************************
 
 }
