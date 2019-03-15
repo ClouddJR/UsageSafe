e28ff08cc37f4f200c90713bf39195ef9c5e050d
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-15 19:00:05 +0100

    move methods for getting app name and icon to different class

--- app/src/main/java/com/clouddroid/usagesafe/adapters/MostUsedAdapter.kt
+++ app/src/main/java/com/clouddroid/usagesafe/adapters/MostUsedAdapter.kt
@@ -4,7 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.clouddroid.usagesafe.R
 import com.clouddroid.usagesafe.models.AppUsageInfo
+import com.clouddroid.usagesafe.utils.PackageInfoUtils.getAppName
+import com.clouddroid.usagesafe.utils.PackageInfoUtils.getRawAppIcon
 import com.clouddroid.usagesafe.utils.TextUtils.getTotalScreenTimeText
 import kotlinx.android.synthetic.main.item_most_used.view.*
 
@@ -14,7 +17,7 @@ class MostUsedAdapter(private val appUsageList: List<AppUsageInfo>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView =
-            LayoutInflater.from(parent.context).inflate(com.clouddroid.usagesafe.R.layout.item_most_used, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_most_used, parent, false)
         return ViewHolder(itemView)
     }
 
@@ -24,31 +27,22 @@ class MostUsedAdapter(private val appUsageList: List<AppUsageInfo>) :
 
     override fun getItemCount() = appUsageList.size
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bind(appUsageInfo: AppUsageInfo) {
             itemView.screenTimeTV.text = itemView.context.getString(
-                com.clouddroid.usagesafe.R.string.most_used_screen_time,
-                getTotalScreenTimeText(appUsageInfo.totalTimeInForeground)
+                R.string.most_used_screen_time,
+                getTotalScreenTimeText(appUsageInfo.totalTimeInForeground, itemView.context)
             )
 
             itemView.launchCountTV.text = itemView.context.getString(
-                com.clouddroid.usagesafe.R.string.most_used_opened_times,
+                R.string.most_used_opened_times,
                 appUsageInfo.launchCount
             )
 
-            itemView.appIconIV.setImageDrawable(
-                itemView.context?.packageManager
-                    ?.getApplicationIcon(appUsageInfo.packageName)
-            )
+            itemView.appIconIV.setImageDrawable(getRawAppIcon(appUsageInfo.packageName, itemView.context))
 
-            val applicationInfo = itemView.context?.packageManager?.getApplicationInfo(appUsageInfo.packageName, 0)
-
-            val applicationName =
-                if (applicationInfo != null) itemView.context?.packageManager?.getApplicationLabel(applicationInfo)
-                else "(unknown)"
-
-            itemView.appTitleTV.text = applicationName
+            itemView.appTitleTV.text = getAppName(appUsageInfo.packageName, itemView.context)
         }
 
     }
--- app/src/main/java/com/clouddroid/usagesafe/utils/PackageInfoUtils.kt
+++ app/src/main/java/com/clouddroid/usagesafe/utils/PackageInfoUtils.kt
@@ -1,15 +1,41 @@
 package com.clouddroid.usagesafe.utils
 
 import android.content.Context
-import android.graphics.drawable.BitmapDrawable
+import android.content.pm.PackageManager
+import android.graphics.drawable.Drawable
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.graphics.drawable.toDrawable
+import com.clouddroid.usagesafe.R
 
 object PackageInfoUtils {
 
-    fun getAppIcon(packageName: String, context: Context?): BitmapDrawable? {
-        val drawable = context?.packageManager?.getApplicationIcon(packageName)
-        val bitmap = drawable?.toBitmap(80, 80)
-        return bitmap?.toDrawable(context.resources)
+    fun getResizedAppIcon(packageName: String, context: Context?): Drawable? {
+        return try {
+            val drawable = context?.packageManager?.getApplicationIcon(packageName)
+            val bitmap = drawable?.toBitmap(80, 80)
+            bitmap?.toDrawable(context.resources)
+        } catch (e: PackageManager.NameNotFoundException) {
+            val defaultDrawable = context?.getDrawable(R.mipmap.ic_launcher_round)
+            val bitmap = defaultDrawable?.toBitmap(80, 80)
+            bitmap?.toDrawable(context.resources)
+        }
+    }
+
+    fun getRawAppIcon(packageName: String, context: Context?): Drawable? {
+        return try {
+            return context?.packageManager?.getApplicationIcon(packageName)
+        } catch (e: PackageManager.NameNotFoundException) {
+            context?.getDrawable(R.mipmap.ic_launcher_round)
+        }
+    }
+
+    fun getAppName(packageName: String, context: Context?): CharSequence? {
+        return try {
+            val applicationInfo = context?.packageManager?.getApplicationInfo(packageName, 0)
+            if (applicationInfo != null) context.packageManager?.getApplicationLabel(applicationInfo)
+            else packageName
+        } catch (e: PackageManager.NameNotFoundException) {
+            packageName
+        }
     }
 }
