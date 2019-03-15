54637744524a4a57f1fb56bb1c701cc09f850597
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-15 19:09:29 +0100

    add option to display seconds in screen time text when total time is less then one minute

--- app/src/main/java/com/clouddroid/usagesafe/utils/TextUtils.kt
+++ app/src/main/java/com/clouddroid/usagesafe/utils/TextUtils.kt
@@ -1,18 +1,19 @@
 package com.clouddroid.usagesafe.utils
 
+import android.content.Context
 import java.util.concurrent.TimeUnit
 
 object TextUtils {
 
-    fun getTotalScreenTimeText(totalScreenTimeMillis: Long): String {
+    fun getTotalScreenTimeText(totalScreenTimeMillis: Long, context: Context?): String {
         var minutes = TimeUnit.MILLISECONDS.toMinutes(totalScreenTimeMillis)
         val hours = minutes / 60
         minutes %= 60
 
-        return if (hours > 0) {
-            "$hours hr, $minutes min"
-        } else {
-            "$minutes min"
+        return when {
+            hours > 0 -> "$hours hr, $minutes min"
+            minutes > 0 -> "$minutes min"
+            else -> "${TimeUnit.MILLISECONDS.toSeconds(totalScreenTimeMillis)} sec"
         }
     }
 }
