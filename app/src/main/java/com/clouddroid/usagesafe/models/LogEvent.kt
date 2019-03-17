ff7bf18974d793120a6df31947b79494a88db09b
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-17 20:43:11 +0100

    add model that represent event in system logs

---
+++ app/src/main/java/com/clouddroid/usagesafe/models/LogEvent.kt
@@ -0,0 +1,17 @@
+package com.clouddroid.usagesafe.models
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class LogEvent : RealmObject() {
+
+    @PrimaryKey
+    var timestamp: Long = 0
+    var packageName: String? = ""
+    var className: String? = ""
+    var eventType = 0
+
+    override fun toString(): String {
+        return "LogEvent (timestamp=$timestamp, packageName='$packageName')"
+    }
+}
