c08ec9e1d8b133e6009406f2b0f2b7fa4661ca6e
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-17 20:30:43 +0100

    add BaseActivity as another target in ApplicationComponent

--- app/src/main/java/com/clouddroid/usagesafe/di/ApplicationComponent.kt
+++ app/src/main/java/com/clouddroid/usagesafe/di/ApplicationComponent.kt
@@ -1,6 +1,7 @@
 package com.clouddroid.usagesafe.di
 
 import com.clouddroid.usagesafe.UsageSafeApp
+import com.clouddroid.usagesafe.activities.BaseActivity
 import com.clouddroid.usagesafe.fragments.BaseFragment
 import dagger.Component
 import javax.inject.Singleton
@@ -9,5 +10,6 @@ import javax.inject.Singleton
 @Component(modules = [ApplicationModule::class, ViewModelsModule::class])
 interface ApplicationComponent {
     fun inject(target: UsageSafeApp)
+    fun inject(target: BaseActivity)
     fun inject(target: BaseFragment)
 }
