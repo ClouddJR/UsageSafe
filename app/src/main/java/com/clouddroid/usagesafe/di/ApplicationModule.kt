78bba50357366d685c1e7e46bb3d08fbf4329fa1
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-17 20:31:16 +0100

    add methods to provide SharedPreferences and DatabaseRepository

--- app/src/main/java/com/clouddroid/usagesafe/di/ApplicationModule.kt
+++ app/src/main/java/com/clouddroid/usagesafe/di/ApplicationModule.kt
@@ -3,8 +3,11 @@ package com.clouddroid.usagesafe.di
 import android.app.Application
 import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
+import com.clouddroid.usagesafe.repositories.DatabaseRepository
+import com.clouddroid.usagesafe.utils.PreferencesUtils.defaultPrefs
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -18,11 +21,19 @@ class ApplicationModule(private val application: Application) {
 
     @Provides
     @Singleton
+    fun provideSharedPreferences(): SharedPreferences = defaultPrefs(application)
+
+    @Provides
+    @Singleton
     fun provideUsageStatsManager(): UsageStatsManager =
         ContextCompat.getSystemService(application, UsageStatsManager::class.java)!!
 
     @Provides
     @Singleton
     fun providePackageManager(): PackageManager = application.packageManager
+
+    @Provides
+    @Singleton
+    fun provideDatabaseRepository(): DatabaseRepository = DatabaseRepository()
 
 }
