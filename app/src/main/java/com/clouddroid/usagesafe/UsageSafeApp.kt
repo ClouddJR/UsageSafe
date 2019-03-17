8d9d85cda21107309c315136d827f929f2130fe5
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-17 20:26:33 +0100

    add Realm initialization in Application class

--- app/src/main/java/com/clouddroid/usagesafe/UsageSafeApp.kt
+++ app/src/main/java/com/clouddroid/usagesafe/UsageSafeApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import com.clouddroid.usagesafe.di.ApplicationComponent
 import com.clouddroid.usagesafe.di.ApplicationModule
 import com.clouddroid.usagesafe.di.DaggerApplicationComponent
+import com.clouddroid.usagesafe.repositories.DatabaseRepository
 
 class UsageSafeApp : Application() {
 
@@ -15,6 +16,15 @@ class UsageSafeApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        injectDependencies()
+        initRealm()
+    }
+
+    private fun initRealm() {
+        DatabaseRepository.RealmInitializer.initRealm(this)
+    }
+
+    private fun injectDependencies() {
         component.inject(this)
     }
 }
