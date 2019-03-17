eac8e479f24888e8852f2566e60d9ba4bf1e7807
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-17 20:36:50 +0100

    add methods to initialize Realm and get its instance and methods to add/get data

--- app/src/main/java/com/clouddroid/usagesafe/repositories/DatabaseRepository.kt
+++ app/src/main/java/com/clouddroid/usagesafe/repositories/DatabaseRepository.kt
@@ -1,4 +1,49 @@
 package com.clouddroid.usagesafe.repositories
 
+import android.content.Context
+import com.clouddroid.usagesafe.models.AppLimit
+import com.clouddroid.usagesafe.models.LogEvent
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
 class DatabaseRepository {
+
+    private var realm: Realm
+    private val config = RealmConfiguration
+        .Builder()
+        .deleteRealmIfMigrationNeeded()
+        .compactOnLaunch()
+        .build()
+
+    object RealmInitializer {
+        fun initRealm(context: Context) {
+            Realm.init(context)
+        }
+    }
+
+    init {
+        realm = Realm.getInstance(config)
+    }
+
+    fun addAppLimit(appLimit: AppLimit) {
+        realm.executeTransactionAsync {
+            it.copyToRealmOrUpdate(appLimit)
+        }
+    }
+
+    fun addLogEvent(logEvents: List<LogEvent>) {
+        logEvents.chunked(500).forEach { list ->
+            realm.executeTransactionAsync {
+                it.insertOrUpdate(list)
+            }
+        }
+    }
+
+    fun getLogEventsFromRange(beginMillis: Long, endMillis: Long): List<LogEvent> {
+        val list = mutableListOf<LogEvent>()
+        realm.executeTransactionAsync {
+            list.addAll(it.where(LogEvent::class.java).between("timestamp", beginMillis, endMillis).findAll())
+        }
+        return list
+    }
 }
