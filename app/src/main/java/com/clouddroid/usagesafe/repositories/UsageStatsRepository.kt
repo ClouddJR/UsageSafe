8eda478c912fe547c472f40e227d1918afe53eaa
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-17 20:37:37 +0100

    add method to get data from last week in UsageStatsRepository

--- app/src/main/java/com/clouddroid/usagesafe/repositories/UsageStatsRepository.kt
+++ app/src/main/java/com/clouddroid/usagesafe/repositories/UsageStatsRepository.kt
@@ -7,8 +7,10 @@ import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import com.clouddroid.usagesafe.models.AppDetails
 import com.clouddroid.usagesafe.models.AppUsageInfo
+import com.clouddroid.usagesafe.models.LogEvent
 import com.clouddroid.usagesafe.utils.PackageInfoUtils.getAppName
 import com.clouddroid.usagesafe.utils.PackageInfoUtils.getRawAppIcon
+import io.reactivex.Observable
 import java.util.*
 import javax.inject.Inject
 
@@ -17,6 +19,39 @@ class UsageStatsRepository @Inject constructor(
     private val packageManager: PackageManager
 ) {
 
+    fun getAppsUsageFromLastWeek(): Observable<List<LogEvent>> {
+
+        return Observable.create<List<LogEvent>> { emitter ->
+
+            val logs = mutableListOf<LogEvent>()
+
+            val beginCalendar = Calendar.getInstance().apply {
+                add(Calendar.DAY_OF_MONTH, -6)
+                set(Calendar.HOUR_OF_DAY, 0)
+                set(Calendar.MINUTE, 1)
+            }
+
+            val endCalendar = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, 23)
+                set(Calendar.MINUTE, 59)
+            }
+
+            val events = usageStatsManager.queryEvents(beginCalendar.timeInMillis, endCalendar.timeInMillis)
+
+            while (events.hasNextEvent()) {
+                val currentEvent = UsageEvents.Event()
+                events.getNextEvent(currentEvent)
+                val logEvent = LogEvent()
+                logEvent.className = currentEvent.className
+                logEvent.timestamp = currentEvent.timeStamp
+                logEvent.packageName = currentEvent.packageName
+                logEvent.eventType = currentEvent.eventType
+                logs.add(logEvent)
+            }
+
+            emitter.onNext(logs)
+        }
+    }
 
     fun getAppsUsageFromToday(): Pair<Map<String, AppUsageInfo>, Int> {
 
@@ -113,16 +148,28 @@ class UsageStatsRepository @Inject constructor(
         }
     }
 
-    fun getListOfAllApps(context: Context): List<AppDetails> {
+    fun getListOfAllApps(context: Context, includeSystemApps: Boolean): List<AppDetails> {
         val appsList = mutableListOf<AppDetails>()
 
         packageManager.getInstalledApplications(PackageManager.GET_META_DATA).forEach { appInfo ->
-            if (appInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
+            if (appInfo.flags and ApplicationInfo.FLAG_SYSTEM == 1) {
+                if (includeSystemApps) {
+                    appsList.add(
+                        AppDetails(
+                            packageName = appInfo.packageName,
+                            name = getAppName(appInfo.packageName, context).toString(),
+                            icon = getRawAppIcon(appInfo.packageName, context),
+                            isSystemApp = true
+                        )
+                    )
+                }
+            } else if (appInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
                 appsList.add(
                     AppDetails(
                         packageName = appInfo.packageName,
                         name = getAppName(appInfo.packageName, context).toString(),
-                        icon = getRawAppIcon(appInfo.packageName, context)
+                        icon = getRawAppIcon(appInfo.packageName, context),
+                        isSystemApp = false
                     )
                 )
             }
