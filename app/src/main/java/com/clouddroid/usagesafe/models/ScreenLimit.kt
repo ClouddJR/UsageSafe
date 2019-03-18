444a2b2cf1690126565c09451729e64cedc57f4a
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-18 12:19:00 +0100

    add id as primary key for screen limit with constant value to keep only one object of this class

--- app/src/main/java/com/clouddroid/usagesafe/models/ScreenLimit.kt
+++ app/src/main/java/com/clouddroid/usagesafe/models/ScreenLimit.kt
@@ -1,7 +1,11 @@
 package com.clouddroid.usagesafe.models
 
 import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
 
 open class ScreenLimit : RealmObject() {
+
+    @PrimaryKey
+    var id: Int = 1 //only one object
     var limitMillis: Long = 0
 }
