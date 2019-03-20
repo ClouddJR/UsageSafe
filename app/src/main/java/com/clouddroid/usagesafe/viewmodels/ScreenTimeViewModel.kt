8e975b1a5ccce2a139be64e82b64be6cdebede60
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-20 11:07:24 +0100

    add ViewModel class for ScreenTimeFragment

---
+++ app/src/main/java/com/clouddroid/usagesafe/viewmodels/ScreenTimeViewModel.kt
@@ -0,0 +1,11 @@
+package com.clouddroid.usagesafe.viewmodels
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class ScreenTimeViewModel @Inject constructor() : ViewModel() {
+
+    fun init() {
+
+    }
+}
