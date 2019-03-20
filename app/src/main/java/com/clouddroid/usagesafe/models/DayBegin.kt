54c3b01bc048da71fce484c93e18ebe779623ddf
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-20 10:04:36 +0100

    add models that identifies user preferences

---
+++ app/src/main/java/com/clouddroid/usagesafe/models/DayBegin.kt
@@ -0,0 +1,12 @@
+package com.clouddroid.usagesafe.models
+
+object DayBegin {
+    const val _12AM = 0
+    const val _1AM = 1
+    const val _2AM = 2
+    const val _3AM = 3
+    const val _4AM = 4
+    const val _5AM = 5
+    const val _6AM = 6
+    const val _7AM = 7
+}
---
+++ app/src/main/java/com/clouddroid/usagesafe/models/WeekBegin.kt
@@ -0,0 +1,7 @@
+package com.clouddroid.usagesafe.models
+
+object WeekBegin {
+    const val SIX_DAYS_AGO = "six"
+    const val SUNDAY = "sunday"
+    const val MONDAY = "monday"
+}
