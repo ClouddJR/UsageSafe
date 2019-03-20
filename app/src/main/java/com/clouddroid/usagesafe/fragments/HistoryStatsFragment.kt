92c2878f2ecead9d68e55022d3391f84ec7dcd79
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-20 10:06:42 +0100

    connect HistoryStatsFragment and ScreenTimeFragment to ViewModels

--- app/src/main/java/com/clouddroid/usagesafe/fragments/HistoryStatsFragment.kt
+++ app/src/main/java/com/clouddroid/usagesafe/fragments/HistoryStatsFragment.kt
@@ -1,18 +1,39 @@
 package com.clouddroid.usagesafe.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.clouddroid.usagesafe.R
 import com.clouddroid.usagesafe.adapters.ViewPagerAdapter
+import com.clouddroid.usagesafe.viewmodels.HistoryStatsViewModel
 import kotlinx.android.synthetic.main.fragment_history_stats.*
 
 class HistoryStatsFragment : BaseFragment() {
 
+    private lateinit var viewModel: HistoryStatsViewModel
+
     override fun getLayoutId() = R.layout.fragment_history_stats
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        initViewModel()
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setUpViewPager()
+        observeData()
+        setOnWeekChangeListener()
+    }
 
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(activity!!, viewModelFactory)[HistoryStatsViewModel::class.java]
+        viewModel.setCurrentWeek(viewModel.getTodayCalendar())
+    }
+
+    private fun setUpViewPager() {
         val viewPagerAdapter = ViewPagerAdapter(childFragmentManager)
 
         viewPagerAdapter.addFragment(ScreenTimeFragment(), "Screen time")
@@ -22,5 +43,21 @@ class HistoryStatsFragment : BaseFragment() {
         viewPager.adapter = viewPagerAdapter
 
         tabLayout.setupWithViewPager(viewPager)
+    }
+
+    private fun observeData() {
+        viewModel.currentWeekText.observe(this, Observer {
+            dateTV.text = it
+        })
+    }
+
+    private fun setOnWeekChangeListener() {
+        arrowRight.setOnClickListener {
+            viewModel.rightArrowClicked()
+        }
+
+        arrowLeft.setOnClickListener {
+            viewModel.leftArrowClicked()
+        }
     }
 }
--- app/src/main/java/com/clouddroid/usagesafe/fragments/ScreenTimeFragment.kt
+++ app/src/main/java/com/clouddroid/usagesafe/fragments/ScreenTimeFragment.kt
@@ -1,8 +1,39 @@
 package com.clouddroid.usagesafe.fragments
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.clouddroid.usagesafe.R
+import com.clouddroid.usagesafe.viewmodels.HistoryStatsViewModel
+import com.clouddroid.usagesafe.viewmodels.ScreenTimeViewModel
 
 class ScreenTimeFragment : BaseFragment() {
 
+    private lateinit var historyStatsViewModel: HistoryStatsViewModel
+    private lateinit var screenTimeViewModel: ScreenTimeViewModel
+
     override fun getLayoutId() = R.layout.fragment_screen_time
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        initViewModels()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeData()
+    }
+
+    private fun initViewModels() {
+        historyStatsViewModel = ViewModelProviders.of(activity!!, viewModelFactory)[HistoryStatsViewModel::class.java]
+        screenTimeViewModel = ViewModelProviders.of(activity!!, viewModelFactory)[ScreenTimeViewModel::class.java]
+    }
+
+    private fun observeData() {
+        historyStatsViewModel.weeklyData.observe(this, Observer {
+
+        })
+    }
 }
