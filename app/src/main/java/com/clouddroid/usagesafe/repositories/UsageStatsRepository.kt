8e0c47dc9720ee921e5e16f266d95b765abd7f9e
Arkadiusz Chmura <arekchmura@gmail.com>
2019-03-24 19:44:37 +0100

    replace UsageStats.Event with LogEvent in every function inside stats repository for better compatibility with other classes

--- app/src/main/java/com/clouddroid/usagesafe/repositories/UsageStatsRepository.kt
+++ app/src/main/java/com/clouddroid/usagesafe/repositories/UsageStatsRepository.kt
@@ -3,20 +3,24 @@ package com.clouddroid.usagesafe.repositories
 import android.app.usage.UsageEvents
 import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import com.clouddroid.usagesafe.models.AppDetails
 import com.clouddroid.usagesafe.models.AppUsageInfo
 import com.clouddroid.usagesafe.models.LogEvent
+import com.clouddroid.usagesafe.utils.PackageInfoUtils
 import com.clouddroid.usagesafe.utils.PackageInfoUtils.getAppName
 import com.clouddroid.usagesafe.utils.PackageInfoUtils.getRawAppIcon
+import com.clouddroid.usagesafe.utils.PreferencesUtils.get
 import io.reactivex.Observable
 import java.util.*
 import javax.inject.Inject
 
 class UsageStatsRepository @Inject constructor(
     private val usageStatsManager: UsageStatsManager,
-    private val packageManager: PackageManager
+    private val packageManager: PackageManager,
+    private val sharedPreferences: SharedPreferences
 ) {
 
     fun getAppsUsageFromLastWeek(): Observable<List<LogEvent>> {
@@ -56,7 +60,7 @@ class UsageStatsRepository @Inject constructor(
     fun getAppsUsageFromToday(): Pair<Map<String, AppUsageInfo>, Int> {
 
         var unlockCount = 0
-        val allEventsList = mutableListOf<UsageEvents.Event>()
+        val allEventsList = mutableListOf<LogEvent>()
         val appUsageMap = mutableMapOf<String, AppUsageInfo>()
 
         val beginCalendar = Calendar.getInstance().apply {
@@ -69,11 +73,13 @@ class UsageStatsRepository @Inject constructor(
             set(Calendar.MINUTE, 59)
         }
 
+        //getting only relevant events out of all logs (getting only MOVE_TO_BACKGROUND or MOVE_TO_FOREGROUND)
         getAllRelevantEventsToList(
             usageStatsManager.queryEvents(beginCalendar.timeInMillis, endCalendar.timeInMillis),
             allEventsList
         )
 
+        //iterating over events to get different stats
         for (i in 0 until allEventsList.size - 1) {
             val first = allEventsList[i]
             val second = allEventsList[i + 1]
@@ -83,10 +89,52 @@ class UsageStatsRepository @Inject constructor(
             increaseTotalTimeInForeground(first, second, appUsageMap)
         }
 
+        //removing launcher if it should not be included
+        val isLauncherIncluded = sharedPreferences["is_launcher_included"] ?: false
+        if (!isLauncherIncluded) {
+            val launcherPackageName = PackageInfoUtils.getDefaultLauncherPackageName(packageManager)
+            appUsageMap.remove(launcherPackageName)
+        }
+
         return Pair(appUsageMap, unlockCount)
     }
 
-    private fun getAllRelevantEventsToList(events: UsageEvents?, allEventsList: MutableList<UsageEvents.Event>) {
+    fun getUsageMapFrom(logs: List<LogEvent>): MutableMap<String, AppUsageInfo> {
+        val relevantLogs = getAllRelevantEventsToList(logs)
+        val appUsageMap = mutableMapOf<String, AppUsageInfo>()
+
+        for (i in 0 until relevantLogs.size - 1) {
+            val first = relevantLogs[i]
+            val second = relevantLogs[i + 1]
+
+            increaseLaunchCount(first, second, appUsageMap)
+            increaseTotalTimeInForeground(first, second, appUsageMap)
+        }
+
+        //removing launcher if it should not be included
+        val isLauncherIncluded = sharedPreferences["is_launcher_included"] ?: false
+        if (!isLauncherIncluded) {
+            val launcherPackageName = PackageInfoUtils.getDefaultLauncherPackageName(packageManager)
+            appUsageMap.remove(launcherPackageName)
+        }
+
+        return appUsageMap
+    }
+
+    fun getNumberOfUnlocksFrom(logs: List<LogEvent>): Int {
+        val relevantLogs = getAllRelevantEventsToList(logs)
+        var numberOfUnlocks = 0
+
+        for (i in 0 until relevantLogs.size - 1) {
+            val first = relevantLogs[i]
+            val second = relevantLogs[i + 1]
+
+            numberOfUnlocks += getPossiblePhoneUnlock(first, second)
+        }
+        return numberOfUnlocks
+    }
+
+    private fun getAllRelevantEventsToList(events: UsageEvents?, allEventsList: MutableList<LogEvent>) {
         events?.let {
             while (events.hasNextEvent()) {
                 val currentEvent = UsageEvents.Event()
@@ -95,13 +143,32 @@ class UsageStatsRepository @Inject constructor(
                 if (currentEvent.eventType == UsageEvents.Event.MOVE_TO_BACKGROUND ||
                     currentEvent.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
                 ) {
-                    allEventsList.add(currentEvent)
+                    allEventsList.add(LogEvent().apply {
+                        timestamp = currentEvent.timeStamp
+                        packageName = currentEvent.packageName
+                        className = currentEvent.className
+                        eventType = currentEvent.eventType
+                    })
                 }
             }
         }
     }
 
-    private fun getPossiblePhoneUnlock(first: UsageEvents.Event, second: UsageEvents.Event): Int {
+    private fun getAllRelevantEventsToList(logs: List<LogEvent>): List<LogEvent> {
+        val list = mutableListOf<LogEvent>()
+
+        logs.forEach { currentEvent ->
+            if (currentEvent.eventType == UsageEvents.Event.MOVE_TO_BACKGROUND ||
+                currentEvent.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
+            ) {
+                list.add(currentEvent)
+            }
+        }
+
+        return list
+    }
+
+    private fun getPossiblePhoneUnlock(first: LogEvent, second: LogEvent): Int {
         return if (first.packageName == second.packageName
             && first.className == second.className
             && first.eventType == UsageEvents.Event.MOVE_TO_BACKGROUND
@@ -114,8 +181,8 @@ class UsageStatsRepository @Inject constructor(
     }
 
     private fun increaseLaunchCount(
-        first: UsageEvents.Event,
-        second: UsageEvents.Event,
+        first: LogEvent,
+        second: LogEvent,
         appUsageMap: MutableMap<String, AppUsageInfo>
     ) {
         if (first.packageName != second.packageName
@@ -132,8 +199,8 @@ class UsageStatsRepository @Inject constructor(
     }
 
     private fun increaseTotalTimeInForeground(
-        first: UsageEvents.Event,
-        second: UsageEvents.Event,
+        first: LogEvent,
+        second: LogEvent,
         appUsageMap: MutableMap<String, AppUsageInfo>
     ) {
         if (first.className == second.className && first.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
@@ -144,7 +211,7 @@ class UsageStatsRepository @Inject constructor(
                     packageName = second.packageName
                 }
             }
-            appUsageMap[second.packageName]!!.totalTimeInForeground += (second.timeStamp - first.timeStamp)
+            appUsageMap[second.packageName]!!.totalTimeInForeground += (second.timestamp - first.timestamp)
         }
     }
 
