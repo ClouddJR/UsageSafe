f06c08d09f4e204e3a6c280dfe3e6e9fcda6eb3f
Arkadiusz Chmura <arekchmura@gmail.com>
2019-04-05 12:46:20 +0200

    add custom marker view for charts

---
+++ app/src/main/java/com/clouddroid/usagesafe/utils/HourMarkerView.kt
@@ -0,0 +1,28 @@
+package com.clouddroid.usagesafe.utils
+
+import android.content.Context
+
+import android.widget.TextView
+import com.clouddroid.usagesafe.R
+import com.clouddroid.usagesafe.viewmodels.DayDetailsViewModel
+import com.github.mikephil.charting.components.MarkerView
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.highlight.Highlight
+
+class HourMarkerView(context: Context, layoutId: Int, private val mode: Int, private val hoursNames: List<String>) :
+    MarkerView(context, layoutId) {
+
+    private var titleTextView: TextView = findViewById(R.id.valueTV)
+
+    override fun refreshContent(e: Entry?, highlight: Highlight?) {
+
+        val marketText = if (mode == DayDetailsViewModel.MODE.SCREEN_TIME) {
+            "${hoursNames[e?.x?.toInt() ?: 0]} : ${TextUtils.getTotalScreenTimeText(e?.y?.toLong() ?: 0L, context)}"
+        } else {
+            "${hoursNames[e?.x?.toInt() ?: 0]} : ${"%.0f".format(e?.y)}"
+        }
+
+        titleTextView.text = marketText
+        super.refreshContent(e, highlight)
+    }
+}
