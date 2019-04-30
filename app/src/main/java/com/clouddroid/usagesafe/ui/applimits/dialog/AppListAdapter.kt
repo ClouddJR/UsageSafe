9cccf8dc00a728ce1152c5953cbdb21ce63bd363
Arkadiusz Chmura <arekchmura@gmail.com>
2019-04-30 18:48:53 +0200

    add AppList fragment that displays list of apps in dialog

---
+++ app/src/main/java/com/clouddroid/usagesafe/ui/applimits/dialog/AppListAdapter.kt
@@ -0,0 +1,61 @@
+package com.clouddroid.usagesafe.ui.applimits.dialog
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.clouddroid.usagesafe.R
+import com.clouddroid.usagesafe.data.model.AppDetails
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.item_app_limit_dialog.view.*
+
+class AppListAdapter(private val context: Context, val onItemClicked: (String) -> Unit) :
+    RecyclerView.Adapter<AppListAdapter.ViewHolder>() {
+
+    private lateinit var disposable: Disposable
+
+    private val appsList = mutableListOf<AppDetails>()
+
+    fun swapItems(appDetailsList: List<AppDetails>) {
+        //dispose previous calculation if exist
+        if (::disposable.isInitialized && !disposable.isDisposed) {
+            disposable.dispose()
+        }
+
+        disposable = Single.fromCallable {
+            DiffUtil.calculateDiff(AppListAdapterDiffUtil(appsList, appDetailsList))
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { diffResult ->
+                appsList.clear()
+                appsList.addAll(appDetailsList)
+                diffResult.dispatchUpdatesTo(this)
+            }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_app_limit_dialog, parent, false))
+
+    override fun getItemCount() = appsList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(appsList[position])
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(appDetails: AppDetails) {
+            Glide.with(context).load(appDetails.icon).into(itemView.appIconIV)
+            itemView.appTitle.text = appDetails.name
+
+            itemView.setOnClickListener {
+                onItemClicked.invoke(appDetails.packageName)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/clouddroid/usagesafe/ui/applimits/dialog/AppListFragment.kt
@@ -0,0 +1,56 @@
+package com.clouddroid.usagesafe.ui.applimits.dialog
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.clouddroid.usagesafe.R
+import com.clouddroid.usagesafe.data.model.AppDetails
+import com.clouddroid.usagesafe.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_app_list_in_dialog.*
+
+class AppListFragment : BaseFragment() {
+
+    private lateinit var viewModel: AppLimitsDialogViewModel
+    private lateinit var adapter: AppListAdapter
+
+    override fun getLayoutId() = R.layout.fragment_app_list_in_dialog
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        initViewModel()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRVAdapter()
+        observeDataChanges()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProviders.of(parentFragment!!, viewModelFactory)[AppLimitsDialogViewModel::class.java]
+        viewModel.getListOfAllApps(context!!)
+    }
+
+    private fun initRVAdapter() {
+        adapter = AppListAdapter(context!!) { packageName ->
+            viewModel.setAppClicked(packageName)
+        }
+
+        appsRV.layoutManager = LinearLayoutManager(context)
+        appsRV.adapter = adapter
+    }
+
+    private fun observeDataChanges() {
+        viewModel.appsList.observe(this, Observer {
+            updateRVItems(it)
+        })
+    }
+
+    private fun updateRVItems(appDetails: List<AppDetails>) {
+        adapter.swapItems(appDetails)
+    }
+}
