17b1ece333ef367048cab7405b8cc3ea7a3d21e9
Arkadiusz Chmura <arekchmura@gmail.com>
2019-05-05 12:30:38 +0200

    create notification channel in App class

--- app/src/main/java/com/clouddroid/usagesafe/UsageSafeApp.kt
+++ app/src/main/java/com/clouddroid/usagesafe/UsageSafeApp.kt
@@ -1,10 +1,15 @@
 package com.clouddroid.usagesafe
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.clouddroid.usagesafe.data.local.DatabaseRepository
 import com.clouddroid.usagesafe.injection.component.ApplicationComponent
 import com.clouddroid.usagesafe.injection.component.DaggerApplicationComponent
 import com.clouddroid.usagesafe.injection.module.ApplicationModule
+import com.clouddroid.usagesafe.util.NotificationUtils.CHANNEL_ID
 
 class UsageSafeApp : Application() {
 
@@ -18,6 +23,7 @@ class UsageSafeApp : Application() {
         super.onCreate()
         injectDependencies()
         initRealm()
+        createNotificationChannel()
     }
 
     private fun injectDependencies() {
@@ -26,5 +32,19 @@ class UsageSafeApp : Application() {
 
     private fun initRealm() {
         DatabaseRepository.RealmInitializer.initRealm(this)
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val name = "App limits"
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.setShowBadge(false)
+            channel.enableVibration(false)
+            channel.enableLights(false)
+
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
