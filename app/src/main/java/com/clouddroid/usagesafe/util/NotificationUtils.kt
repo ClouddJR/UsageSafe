66ea7780a57dec9f50e521bc4dc13f5539a27d48
Arkadiusz Chmura <arekchmura@gmail.com>
2019-05-05 12:30:14 +0200

    add util class for notifications

---
+++ app/src/main/java/com/clouddroid/usagesafe/util/NotificationUtils.kt
@@ -0,0 +1,6 @@
+package com.clouddroid.usagesafe.util
+
+object NotificationUtils {
+    const val CHANNEL_ID = "usage_monitor_channel"
+    const val APP_USAGE_MONITOR_SERVICE_NOTIFICATION_ID = 1
+}
