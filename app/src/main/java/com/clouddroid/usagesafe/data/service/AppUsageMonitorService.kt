49d73aab772a107b6e136ad18495f31d46c8f3c7
Arkadiusz Chmura <arekchmura@gmail.com>
2019-05-08 20:44:17 +0200

    add foreground service that observe app limits

--- app/src/main/java/com/clouddroid/usagesafe/data/service/AppUsageMonitorService.kt
+++ app/src/main/java/com/clouddroid/usagesafe/data/service/AppUsageMonitorService.kt
@@ -13,10 +13,14 @@ import com.clouddroid.usagesafe.UsageSafeApp
 import com.clouddroid.usagesafe.data.local.DatabaseRepository
 import com.clouddroid.usagesafe.data.local.UsageStatsRepository
 import com.clouddroid.usagesafe.data.model.AppLimit
+import com.clouddroid.usagesafe.data.model.AppUsageInfo
+import com.clouddroid.usagesafe.data.model.LogEvent
+import com.clouddroid.usagesafe.ui.appblocking.BlockingActivity
 import com.clouddroid.usagesafe.ui.main.MainActivity
 import com.clouddroid.usagesafe.util.NotificationUtils
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import java.util.*
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
@@ -29,13 +33,129 @@ class AppUsageMonitorService : Service() {
     @Inject
     lateinit var usageStatsRepository: UsageStatsRepository
 
-    var appLimitsList = listOf<AppLimit>()
+    private val compositeDisposable = CompositeDisposable()
+
+    private var foregroundAppPackageName = ""
+    private var appUsageMap = mutableMapOf<String, AppUsageInfo>()
+    private var appLimitsList = listOf<AppLimit>()
 
     override fun onCreate() {
         super.onCreate()
-        (application as UsageSafeApp).component.inject(this)
-        observeUsageLogs()
+        injectDependencies()
+        updateAppUsageMapPeriodically()
+        watchForLaunchingAppWithLimitExceeded()
         Log.d(AppUsageMonitorService::class.java.name, "Service creation")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotification()
+        updateAppList()
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+        Log.d(AppUsageMonitorService::class.java.name, "Service destroyed")
+    }
+
+    private fun injectDependencies() {
+        (application as UsageSafeApp).component.inject(this)
+    }
+
+    //updating app usage map every 2 minutes
+    private fun updateAppUsageMapPeriodically() {
+        compositeDisposable.add(Observable.interval(0, 2, TimeUnit.MINUTES).flatMap {
+            Observable.fromCallable {
+                usageStatsRepository.getLogsFromToday()
+            }
+        }
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext { logs -> appUsageMap = usageStatsRepository.getAppsUsageMapFromLogs(logs) }
+            .subscribe { logs ->
+                Log.d(AppUsageMonitorService::class.java.name, "Updating map usage")
+                checkIfAppInForegroundShouldBeBlocked(logs)
+            }
+
+        )
+    }
+
+    //checking every 2 seconds if app that has a limit set was launched
+    private fun watchForLaunchingAppWithLimitExceeded() {
+        compositeDisposable.add(Observable.interval(0, 2, TimeUnit.SECONDS)
+            .flatMap {
+                Observable.fromCallable {
+                    val beginCalendar = Calendar.getInstance().apply {
+                        add(Calendar.SECOND, -2)
+                    }
+                    val endCalendar = Calendar.getInstance()
+
+                    usageStatsRepository.getLogsFromRange(beginCalendar.timeInMillis, endCalendar.timeInMillis)
+                }
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext { logs ->
+                //updating information about current app in foreground
+                foregroundAppPackageName =
+                    if (logs.isNotEmpty()) logs.last().packageName else foregroundAppPackageName
+
+                Log.d(
+                    AppUsageMonitorService::class.java.name,
+                    "Current app in foreground: $foregroundAppPackageName"
+                )
+            }
+            .map { logs ->
+                //filtering logs to get only those related to app launches that have a limit set by user
+                //and those that have their limit exceeded
+                logs.filter { logEvent ->
+                    logEvent.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND && appLimitsList.any { appLimit ->
+                        appLimit.packageName == logEvent.packageName
+                                && appLimit.currentLimit <= appUsageMap[logEvent.packageName]?.totalTimeInForeground ?: 0
+                    }
+                }
+            }
+            .flatMap { list -> Observable.just(list.isNotEmpty()) }
+            .subscribe { appShouldBeBlocked ->
+                if (appShouldBeBlocked) {
+                    displayBlockingActivity()
+                }
+                applicationContext
+            })
+    }
+
+    private fun checkIfAppInForegroundShouldBeBlocked(logsList: List<LogEvent>) {
+        val appLimit = appLimitsList.find { it.packageName == foregroundAppPackageName }
+        Log.d(AppUsageMonitorService::class.java.name, "Package name to block possibly: ${appLimit?.packageName}")
+        appLimit?.let {
+            val timeOfForegroundAppArriving = logsList.last().timestamp
+            val amountOfTimeSinceArriving = Calendar.getInstance().timeInMillis - timeOfForegroundAppArriving
+
+            val foregroundAppUsageTime = appUsageMap[foregroundAppPackageName]?.totalTimeInForeground ?: 0
+            Log.d(
+                AppUsageMonitorService::class.java.name,
+                "Time usage of app to be blocked possibly: ${foregroundAppUsageTime + amountOfTimeSinceArriving}"
+            )
+
+            //if today's usage plus current time spent in foreground is more than a limit, we should block this
+            if (logsList.last().packageName == foregroundAppPackageName
+                && foregroundAppUsageTime + amountOfTimeSinceArriving >= it.currentLimit
+            ) {
+                //add time spent in foreground to app usage map
+                //otherwise we would have to wait couple of minutes for that map to be updated
+                appUsageMap[foregroundAppPackageName]?.totalTimeInForeground =
+                    appUsageMap[foregroundAppPackageName]?.totalTimeInForeground?.plus(amountOfTimeSinceArriving) ?: 0
+                displayBlockingActivity()
+            }
+        }
+    }
+
+    private fun displayBlockingActivity() {
+        Intent(this, BlockingActivity::class.java).also { intent ->
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            applicationContext?.startActivity(intent)
+        }
     }
 
     private fun createNotification() {
@@ -57,37 +177,5 @@ class AppUsageMonitorService : Service() {
     private fun updateAppList() {
         appLimitsList = databaseRepository.getListOfLimits()
     }
-
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        createNotification()
-        updateAppList()
-        return START_STICKY
-    }
-
-    private fun observeUsageLogs() {
-        val observable = Observable.interval(0, 3, TimeUnit.SECONDS).flatMap {
-            Observable.fromCallable {
-                val beginCalendar = Calendar.getInstance().apply {
-                    add(Calendar.SECOND, -3)
-                }
-                val endCalendar = Calendar.getInstance()
-                usageStatsRepository.getLogsFromRange(beginCalendar.timeInMillis, endCalendar.timeInMillis)
-            }
-        }
-            .observeOn(AndroidSchedulers.mainThread())
-            .map { list ->
-                list.filter { logEvent ->
-                    logEvent.eventType == UsageEvents.Event.MOVE_TO_FOREGROUND
-                            && appLimitsList.any { appLimit -> appLimit.packageName == logEvent.packageName }
-                }
-            }
-            .subscribe {
-                Log.d(AppUsageMonitorService::class.java.name, "Interval test")
-                Log.d(AppUsageMonitorService::class.java.name, it.toString())
-                applicationContext
-            }
-    }
-
-    override fun onBind(intent: Intent?): IBinder? = null
 
 }
