26fdb97dd8a72321401cb91583709112858d34f5
Arkadiusz Chmura <arekchmura@gmail.com>
2019-05-09 09:53:30 +0200

    add method to get repository object in component

--- app/src/main/java/com/clouddroid/usagesafe/injection/component/ApplicationComponent.kt
+++ app/src/main/java/com/clouddroid/usagesafe/injection/component/ApplicationComponent.kt
@@ -1,6 +1,7 @@
 package com.clouddroid.usagesafe.injection.component
 
 import com.clouddroid.usagesafe.UsageSafeApp
+import com.clouddroid.usagesafe.data.local.DatabaseRepository
 import com.clouddroid.usagesafe.data.service.AppUsageMonitorService
 import com.clouddroid.usagesafe.injection.module.ApplicationModule
 import com.clouddroid.usagesafe.injection.module.ViewModelsModule
@@ -13,9 +14,17 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [ApplicationModule::class, ViewModelsModule::class])
 interface ApplicationComponent {
+
+    //injection targets
+
     fun inject(target: UsageSafeApp)
     fun inject(target: BaseActivity)
     fun inject(target: BaseFragment)
     fun inject(target: BaseDialogFragment)
     fun inject(target: AppUsageMonitorService)
+
+
+    //objects
+
+    fun getDatabaseRepository(): DatabaseRepository
 }
