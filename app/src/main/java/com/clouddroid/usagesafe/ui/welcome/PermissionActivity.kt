a4c4bbfb43347059efb7f82f6011be81acc962a7
Arkadiusz Chmura <arekchmura@gmail.com>
2019-07-16 17:07:19 +0200

    add safe animation in permission activity

--- app/src/main/java/com/clouddroid/usagesafe/ui/welcome/PermissionActivity.kt
+++ app/src/main/java/com/clouddroid/usagesafe/ui/welcome/PermissionActivity.kt
@@ -1,9 +1,13 @@
 package com.clouddroid.usagesafe.ui.welcome
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.provider.Settings
 import androidx.lifecycle.ViewModelProviders
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import com.clouddroid.usagesafe.R
 import com.clouddroid.usagesafe.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_permission.*
@@ -12,17 +16,31 @@ import org.jetbrains.anko.longToast
 
 class PermissionActivity : BaseActivity() {
 
+    private var lockAvd: AnimatedVectorDrawableCompat? = null
+    private var safeAvd: AnimatedVectorDrawableCompat? = null
     private lateinit var viewModel: PermissionActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_permission)
         initViewModel()
+        initAvDs()
         setOnClickListeners()
+        scheduleAnimation()
     }
 
     private fun initViewModel() {
         viewModel = ViewModelProviders.of(this, viewModelFactory)[PermissionActivityViewModel::class.java]
+    }
+
+    private fun initAvDs() {
+        lockAvd = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_lock)
+        lockAvd?.registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
+            override fun onAnimationEnd(drawable: Drawable?) {
+                playSafeShake()
+            }
+        })
+        safeAvd = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_safe)
     }
 
     private fun setOnClickListeners() {
@@ -32,5 +50,26 @@ class PermissionActivity : BaseActivity() {
             startActivity(intent)
             longToast("Find UsageSafe here and grant the permission")
         }
+
+        safeIV.setOnClickListener {
+            playLockRotation()
+        }
+    }
+
+    private fun scheduleAnimation() {
+        val handler = Handler()
+        handler.postDelayed({
+            playLockRotation()
+        }, 500)
+    }
+
+    private fun playLockRotation() {
+        safeIV.setImageDrawable(lockAvd)
+        lockAvd?.start()
+    }
+
+    private fun playSafeShake() {
+        safeIV.setImageDrawable(safeAvd)
+        safeAvd?.start()
     }
 }
